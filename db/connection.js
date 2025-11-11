ad7847f89cbadf0968631d3cb3559d589d856d54
prasadchitalepc <120631210+prasadchitalepc@users.noreply.github.com>
2025-11-11 16:08:06 +0530

    Add files via upload

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,7 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
-
-const ATLAS_URI="mongodb+srv://prasad:pass123@cluster0.1vyyk.mongodb.net/?appName=Cluster0"
+//Following is your connection URI
+//Make sure to replace with your connection string.
+const ATLAS_URI="mongodb+srv://fsddemo_db_user:cY5HRXnMWDL7GtBj@cluster0.3pwyhbs.mongodb.net/?appName=Cluster0"
 
 const uri = ATLAS_URI;
 const client = new MongoClient(uri, {
@@ -26,3 +27,5 @@ try {
 let db = client.db("students");
 
 export default db;
+
+//to run node index.js
